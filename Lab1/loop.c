4db2340f8836c3c0d0e2ea6db7ea8f21071670f4
Steven Wong <syw14@sfu.ca>
2022-05-24 18:46:49 -0700

    Add ungraded exercises

--- Lab1/loop.c
+++ Lab1/loop.c
@@ -1,12 +1,13 @@
 /*
- * Filename: moreScanf.c
+ * Filename: loop.c
  *
  * Description: Reads a floating point value from stdin, then outputs (in this order):
  *      1. The smallest integer larger than or equal to its value.
  *      2. The nearest integer to this value, with hallway values rounded away from zero.
  *      3. The largest integer smaller than or equal to this value.
  * 
- * Adds conditionals as compared to values.c to check mis-behaved input from the user and explains what went wrong.
+ * Adds a while loop as compared to values.c to continously check for input while ending program 
+ * if a mis-behaved input from user is received.
  *
  * Author: Steven Wong
  * Date: May 27, 2022
@@ -18,17 +19,13 @@
 int main(void) {
     float value = 0;
     printf("Please, enter a floating point value: ");
-    int result = scanf("%f", &value);
 
-    if (result ==1) { 
+    while ((scanf("%f", &value) == 1)) { 
         printf("The smallest integer larger than or equal to %.6f is %d.\n", value, (int)ceil(value));  // decimal places of ceiling
         printf("The nearest integer to %.6f is %d.\n", value, (int)round(value));  // decimal places of round
         printf("The largest integer smaller than or equal to %.6f is %d.\n", value, (int)floor(value));  // decimal places of floor
-    } else if (result == 0) { 
-        printf("scanf error: (%d) - No fields were assigned.\n", result);
-    } else { 
-        printf("\nscanf error: (%d) - EOF reached.\n", result);
+        printf("Please, enter a floating point value: ");
     }
-
+    printf("Done.\n");
     return 0;
 }
