893f1c97a701fe326d3f6b03559ebaa817be04c7
Steven Wong <syw14@sfu.ca>
2022-06-09 14:23:16 -0700

    Change slope calculation

--- Lab3/pyramid_2.c
+++ Lab3/pyramid_2.c
@@ -14,41 +14,38 @@
  *               #######
  *
  * Author: Steven Wong
- * Date: June 7, 2022
+ * Date: June 9, 2022
  */
 
 #include <stdio.h>
 #include <math.h> 
 
+#include <stdio.h>
+#include <math.h> 
+
 int main(int argc, char **argv) {
-    // char vars[] = {"width (x)", "breadth (y)", "height (z)"}; 
-    int dimensions[3] = {0}; 
+    const int SIZE = 3;
+    int dimensions[SIZE] = {0}; 
     int width = 7; 
     int breadth = 7; 
     int height = 4; 
 
-
-    // Validation of integers within range required? Check before submission.
     printf("Enter your width, breadth, and height values [0-50], separated by one or more spaces: ");
-    for (int i = 0; i < 3; ++i) { 
-        scanf("%d", &dimensions[i]);
-    }
+    // for (int i = 0; i < 3; ++i) { 
+    //     scanf("%d", &dimensions[i]);
+    // }
 
     for (int row = height; row > 0; --row) { 
-        for (int column = 0; column < width; ++column) {
-            // print spaces/2 of width
-            for (int i = 0; i <2; i++) { 
-                printf(" ");
-            }
-
-            for (int i = 0; i < width; i++) {
-                printf("#");
-            }
-
-            for (int i = 0; i < 2; i++) { 
-                printf(" ");
-            }
         printf("\n");
+        for (int i = 0; i < row; i++) { 
+            printf(" ");
+        }
+        for (int i = 0; i < width-row-1; i++) {
+            printf("#");
+        }
+        for (int i = 0; i < row; i++) { 
+            printf(" ");
         }
     }
+    printf("\n");
 }
