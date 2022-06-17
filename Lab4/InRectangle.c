0695bdc28c07ba74339d5538723fd2258a524714
Steven Wong <syw14@sfu.ca>
2022-06-16 19:41:18 -0700

    Final revision for assignment submission

--- Lab4/InRectangle.c
+++ Lab4/InRectangle.c
@@ -1,11 +1,9 @@
 /*
- * Filename: counting.c
+ * Filename: InRectangle.c
  *
  * Description: Determines whether a point lies inside a rectangle. The point and rectangle
  *              are both specified using arrays of floating point values.
  * 
- * 
- *
  * Author: Steven Wong
  * Date: June 15, 2022
  */
@@ -16,73 +14,34 @@
 #define CORNERS 4
 
 
+// Get the max of two floating point values
 float max(float num1, float num2) { 
     return (num1 > num2 ) ? num1 : num2;
 }
 
+
+// Get the min of two floating point values
 float min(float num1, float num2) { 
     return (num1 < num2 ) ? num1 : num2;
 }
 
+
+// Check if a point is within the range of a rectangle in a X-Y coordinate
 int InRectangle (float pt[POINTS], float rect[CORNERS]) { 
-    float pointX = pt[0];
-    float pointY = pt[1];
-    float upperPointX = max(rect[0], rect[2]);
-    float upperPointY = max(rect[1], rect[3]);
-    float lowerPointX = min(rect[0], rect[2]);
-    float lowerPointY = min(rect[1], rect[3]);
+  
+  // Initialize floats values to check if point is in rectangle
+  const float pointX = pt[0];
+  const float pointY = pt[1];
+  const float upperPointX = max(rect[0], rect[2]);
+  const float upperPointY = max(rect[1], rect[3]);
+  const float lowerPointX = min(rect[0], rect[2]);
+  const float lowerPointY = min(rect[1], rect[3]);
 
-    if (pointX <= upperPointX && pointX >= lowerPointX) { 
-        if (pointY <= upperPointY && pointY >= lowerPointY) { 
-            return 1;
-        }
+  // If point X and point Y are within the bounds of the rectangle points, it must be in the rectangle
+  if (pointX <= upperPointX && pointX >= lowerPointX) { 
+    if (pointY <= upperPointY && pointY >= lowerPointY) { 
+      return 1;
     }
-    return 0;
-}
- 
-
-
-int main( int argc, char* argv[] ) {
-  // Define a rectangle from (1,1) to (2,2)
-  float rect[CORNERS] = {1.0, 1.0, 2.0, 2.0 };
- 
-  // Test Case 1
-  // Define a point that is inside the rectangle
-  float p_in[POINTS] = { 1.5, 1.5 };
-  printf( "Test Case 1\n" );
-  printf( "\tTest Data: rectangle: (1.0,1.0) and (2.0,2.0), point: (1.5,1.5)\n" );
-  printf( "\tExpected Result: point is in the rectangle!\n" );
-  if( InRectangle( p_in, rect ) == 0 ) {
-    puts( "\nTest Case 1 - error: should return true for p_in." );
-    return 1; // indicate error
   }
-  else printf( "\tTest Case 1 - Actual Result: point is in the rectangle!\n" );
- 
-  // Test Case 2
-  // Define a point that is outside the rectangle
-  float p_out[POINTS] = {2.5, 0.5};
-  printf( "Test Case 2\n" );
-  printf( "\tTest Data: rectangle: (1.0,1.0) and (2.0,2.0), point: (2.5,0.5)\n" );
-  printf( "\tExpected Result: point is outside the rectangle!\n" );
-  if( InRectangle( p_out, rect ) != 0 ) {
-  
-    puts( "\nTest Case 2 - error: should return false for p_out." );
-    return 1; // indicate error
-  }
-  else printf( "\tTest Case 2 - Actual Result: point is outside the rectangle!\n" );
- 
-  // Test Case 3
-  // Define a point that is on the edge of the rectangle
-  float p_edge[POINTS] = {1.0, 1.0};
-  printf( "Test Case 3\n" );
-  printf( "\tTest Data: rectangle: (1.0,1.0) and (2.0,2.0), point: (1.0,1.0)\n" );
-  printf( "\tExpected Result: point is on the edge of the rectangle!\n" );
-  if( InRectangle( p_edge, rect ) == 0 ) {
-  
-    puts( "\nTest Case 3 - error: should return true for p_edge." );
-    return 1; // indicate error
-  }
-  else printf( "\tTest Case 3 - Actual Result: point is on the edge of the rectangle!\n" );
- 
-  return 0; // all tests passed
+  return 0;  
 }
--- Lab4/counting.c
+++ Lab4/counting.c
@@ -1,7 +1,7 @@
 /*
  * Filename: counting.c
  *
- * Description: Counts the number of characters, words and lines read from standard input (stdin) until EOF is reached
+ * Description: Counts the number of characters, words and lines read from standard input (stdin) until EOF
  * 
  * 
  * Sample Input: paul
@@ -13,25 +13,26 @@
  * Sample Output: 23 4 4
  *
  * Author: Steven Wong
- * Date: June 15, 2022
+ * Date: June 16, 2022
  */
 
 
 #include<stdio.h> 
-#include<stdbool.h>
-#include<ctype.h>
 
-// Check if char is a letter in the alphabet or apostrophy
-bool isLetterInWord(char character) {
+
+// Check if a given char is a letter in the alphabet or an apostrophy
+int isLetterInWord(char character) {
     // ASCII 39 - Apostrophy
     // ASCII 65 - 90 A-Z
     // ASCII 97 - 122 a-z
+
     const int apostrophy = 39;
     const int capitalA = 65;
     const int capitalZ = 90; 
     const int lowerA = 97;
     const int lowerZ = 122;
 
+    // Typecast char to an integer representation 
     const int ascii = (int)character; 
     if (ascii == apostrophy) { 
         return 1;
@@ -49,34 +50,40 @@ bool isLetterInWord(char character) {
 
 
 void letterFreq(void) {
+    // Initialize counter variables for chars, words, and lines
     unsigned long charCount = 0;
     unsigned long wordCount = 0;
     unsigned long lineCount = 0;
 
+    // Get first char
     char symbol = getchar();
 
     while (symbol != EOF) {
+        // Increment char count in every iteration
         charCount++;
         char nextSymbol = getchar();
 
+        // If previous char is a letter in a word and the current char is not, increment word count
         if (isLetterInWord(symbol) && !isLetterInWord(nextSymbol)) { 
             wordCount++;
         }
 
+        // If current symbol is newline, increment line count
         if(symbol == '\n') { 
             lineCount++;
         }
+
+        // Update previous char
         symbol = nextSymbol;
     }
-    printf("Char count: %lu \nWord Count: %lu \nLine count: %lu\n", charCount, wordCount, lineCount);
+    // Print totals of all counters
+    printf("%lu %lu %lu\n", charCount, wordCount, lineCount);
+
 }
 
-int main(int argc, char **argv) {
-    // Testing isLetterIn Word Function
-    // char letter = 'p'; 
-    // if (isLetterInWord(letter)) { 
-    //     printf("true\n");
 
+// Start counting chars, words, and lines
+int main(int argc, char **argv) {
     letterFreq();
     return 0;
 }
