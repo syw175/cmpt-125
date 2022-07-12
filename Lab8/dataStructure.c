b5620c9c94589b6591ef6a7718b6d98c499ae16b
Steven Wong <syw14@sfu.ca>
2022-07-12 14:07:17 -0700

    ready for final review before submission

--- Lab8/dataStructure.c
+++ Lab8/dataStructure.c
@@ -26,7 +26,7 @@
 #include "dataStructure.h"
 
 
-// Ready for final checking
+// Ready for Final Checks
 /* 
  * Description: Creates a new empty data structure of list_t type and 
  *              if successful, returns a pointer to it. 
@@ -52,7 +52,7 @@ list_t* list_create( void ) {
 }
 
 
-//TO-DO - FUNCTION STUB
+// Ready for Final Checks
 /*
  * Description: Frees all memory used by the data structure pointed to by "list". 
  *              Returns OK.
@@ -74,20 +74,22 @@ result_t list_destroy( list_t* list ) {
 
   // Does this work?
   element_t *currentNode = list->head;
-  while (list->head != list->tail) { 
+  while (currentNode != NULL) { 
     element_t *nextNode = currentNode->next;
     free(currentNode);
-    list->head = nextNode;
+    currentNode = nextNode;
     list->elementCount--;
   }
 
   // Free the memory allocated to the list
+  // free(currentNode);
+  // free(currentNode);
   free(list);
   return OK;
 }
 
 
-// Ready for final checking
+// Ready for Final Checks
 /* 
  * Description: Returns a pointer to a new element i.e., a node  
  *              containing "newElement" and a next-pointer set to NULL, 
@@ -109,8 +111,7 @@ element_t* element_create( int newElement ) {
 }
 
 
-
-// TO-DO - FUNCTION STUB
+// Ready for Final Checks
 /* Description: Appends a new element, i.e., a node containing "newElement", 
  *              to this data structure pointed to by "list" and returns OK. 
  *              If "newElement" cannot be appended, leaves the 
@@ -120,14 +121,38 @@ element_t* element_create( int newElement ) {
  */
 result_t list_append( list_t* list, int newElement ) {
 	
-  result_t result = OK;
-  
-  printf( "Calling list_append(...): appending newElement %d to list.\n", newElement );
-  
-  return result;
+  // Check if the list is NULL, return error
+  if (list == NULL) { 
+    return NULL_PARAM;
+  }
+
+  // Create a new node with the integer newElement
+  element_t *toAppendElement = element_create(newElement);
+
+  // If memory allocation for new element failed, return ERROR
+  if (toAppendElement == NULL) { 
+    return ERROR;
+  }
+
+  // If list is empty, set head to the new element
+  if (list->head == NULL) { 
+    list->head = toAppendElement;
+  }
+
+  // If list is not empty, append to the end of the list
+  if (list->tail) {
+    list->tail->next = toAppendElement;
+  }
+
+  // Set the tail to the new element
+  list->tail = toAppendElement;
+  // Increment element count 
+  list->elementCount++;
+  return OK;
 }
 
 
+// Ready for Final Checks
 /* Description: Prepends a new element, i.e., a node containing "newElement", 
  *              to this data structure pointed to by "list" and returns OK. 
  *              If "newElement" cannot be prepended, leaves the 
@@ -161,7 +186,8 @@ result_t list_prepend( list_t* list, int newElement ) {
 }
 
 
-//TO-DO FUNCTION STUB
+// Ready for Final Checks
+// Revision: What happens if there's only 1 element? Do I have to do anything with the tail?
 /* Description: Removes the front (first) element (node) in the data 
  *              structure pointed to by "list" and returns OK. 
  *              If no elements (no nodes) can be removed, leaves the 
@@ -170,16 +196,32 @@ result_t list_prepend( list_t* list, int newElement ) {
  * Time efficiency: O(1)
  */
 result_t list_removeFront( list_t* list ) {
-  result_t result = OK;
 
-  printf( "Calling list_removeFront(...): remove first element at the fron of the list.\n" );
-  
-  return result;
+  // Check if the list is NULL, return NULL_PARAM
+  if (list == NULL) { 
+    return NULL_PARAM;
+  }
+
+  // Check if the list is empty, return ERROR
+  if (list->elementCount <= 0) { 
+    return ERROR;
+  }
+
+  // Get a pointer to the next element from the head
+  element_t *toBeRemoved = list->head;
+
+  // Set the head to the next element
+  list->head = toBeRemoved->next;
+  // Free the memory allocated to the element
+  free(toBeRemoved);  
+
+  // Decrement element count
+  list->elementCount--;
+  return OK;
 }
 							
 
-
-// TO-DO FUNCTION STUB
+// Ready for Final Checks
 /* Description: Returns a pointer to the element (i.e., to the node) at 
  *              "position" in the data structure pointed to by "list". 
  *              The head of "list" is at "position" 0, the first element 
@@ -190,15 +232,41 @@ result_t list_removeFront( list_t* list ) {
  */ 
 element_t* list_get( list_t* list, unsigned int position ){
 
+  // Initialize the element to NULL
   element_t* anElement = NULL;
 
-  printf( "Calling list_get(...): getting element (node) at position %u in the list.\n", position );    
-    
+  // Check if the list is NULL, return NULL
+  if (list == NULL) { 
+    return anElement;
+  }
+
+  // Check if the position is out of range, return NULL
+  if (list->elementCount <= position) {
+    return anElement;
+  }
+
+  // Set the element to the head of the list
+  int currentIndex = 0;
+  element_t *currentElement = list->head;
+
+  // Loop through the list until the element is found
+  while (currentElement != NULL) { 
+    // If the current index is the position, set the element to the current element and return it
+    if (currentIndex == position) { 
+      anElement = currentElement; 
+      return anElement;
+    }
+    // Increment the current index and set the current element to the next element
+    currentElement = currentElement->next;
+    currentIndex++;
+  }
+
+  // Return the element if found, return NULL if not found
   return anElement;
 }
 
 
-// ASSUME THAT THIS WORKS FOR NOW
+// Ready for Final Checks
 /* Description: Returns a pointer to the element (i.e., to the node) that 
  *              contains the first occurrence of "targetElement" in the data 
  *              structure pointed to by "list". Returns NULL if "targetElement"
@@ -234,6 +302,7 @@ element_t* list_find( list_t* list, int targetElement ) {
 }
 
 
+// Ready for Final Checks
 /* Description: Prints the content of the data structure pointed to by "list",
  *              in human-readable form from the first element
  *              to the last element, between curly braces, then returns OK.
--- Lab8/testDriver.c
+++ Lab8/testDriver.c
@@ -1,15 +1,20 @@
 // This is a test driver 
+// Name: Steven Wong
+// Date: July 11, 2022
+
 
 #include <stdio.h>  // for printf
 #include <stdlib.h> // for malloc + free
 #include "dataStructure.h"  // For Linked List functions
 
 
-
 int main (void) { 
+
     // Create an empty Linked List and verify that mem was allocated successfully
     printf("Now testing the list_create() function...\n");
+    // Create a new empty linked list
     list_t *testDoublyHeadedLL = list_create(); 
+    // Check if the list was created successfully
     if (testDoublyHeadedLL == NULL) { 
         printf("list_create() failed to malloc... exiting now\n");
         return 1;
@@ -32,6 +37,7 @@ int main (void) {
 
     // Creating a new element node 
     printf("Now testing the element_t create() function with value of 5...\n");
+    // Create a new element node with value of 5
     element_t *newNode = element_create(5);
     if (newNode == NULL) { 
         printf("element_t create() failed to create a new element... exiting now\n");
@@ -47,6 +53,7 @@ int main (void) {
 
     // Adding an element to the front of the Linked List 
     printf("Now testing the list_prepend() function with a value of 150...\n");
+    // If prepend is not successful, return error
     if (list_prepend(testDoublyHeadedLL, 150) != OK) { 
         printf("list_prepend() failed... exiting now\n"); 
         return 1;
@@ -55,18 +62,21 @@ int main (void) {
         printf("list_prepend() successfully added the element 5 to the Linked List\n");
         printf("The new contents of the Linked List is...\n");
         // Issues running list_print() on clang, working on test machines
-        // list_print(testDoublyHeadedLL);
+        list_print(testDoublyHeadedLL);
         printf("\n\n");
     }
 
+
     // Checking if a target element can be found in a Linked List 
-    printf("Now testing list_find function with a value of 150\n");
+    printf("Now testing list_find function with a value of 150. Expected: found\n");
+    // If find is not successful, return error
     element_t *searchResult = list_find(testDoublyHeadedLL, 150);
     if (searchResult == NULL) { 
-        printf("Target was not found\n");
+        printf("Actual: Target was not found\n");
     } else { 
-        printf("The target was successfully found.\nThe element_t has an address of %p\n", searchResult);
+        printf("Actual: The target was successfully found.\nThe element_t has an address of %p\n\n", searchResult);
     }
+
 
     // Verifying that a non-existant element cannot be found in a linkedlist 
     // Invstigate why this does not work with clang (it works with gcc)
@@ -74,35 +84,146 @@ int main (void) {
     printf("Now testing list_find function with a value of 72. Expected: Not found!\n"); 
     element_t *newSearch = list_find(testDoublyHeadedLL, 72);
     if (newSearch == NULL) { 
-        printf("Target was not found\n");
+        printf("Actual: Target was not found\n\n");
     } else { 
-        printf("The target was successfully found.\nThe element_t has an address of %p\n", searchResult);
+        printf("Actual: The target was successfully found.\nThe element_t has an address of %p\n\n", searchResult);
     }
 
-    // Destroying a linkedlist
-    printf("Now testing list_destroy() function\n");
-    result_t destroyResult= list_destroy(testDoublyHeadedLL);
-    if (destroyResult == OK) { 
-        printf("Linked List successfully destroyed\n\n"); 
-    } else { 
+
+    // Prepending additional elements to the linked list
+    printf("Now prepending 15, 25, 35, 45 95\nExpected result: { 95 45 35 25 150 }\n");
+    list_prepend(testDoublyHeadedLL, 15);
+    list_prepend(testDoublyHeadedLL, 25);
+    list_prepend(testDoublyHeadedLL, 35);
+    list_prepend(testDoublyHeadedLL, 45);
+    list_prepend(testDoublyHeadedLL, 95);
+    list_print(testDoublyHeadedLL);
+
+
+    // Adding to the end of a linked list
+    printf("Now appending 100 101 102 103\nExpected result: { 95 45 35 25 15 150 100 101 102 103 }\n");
+    list_append(testDoublyHeadedLL, 100);
+    list_append(testDoublyHeadedLL, 101);
+    list_append(testDoublyHeadedLL, 102);
+    list_append(testDoublyHeadedLL, 103);
+    list_print(testDoublyHeadedLL);
+    printf("\n");
+
+
+    // Getting an element from a linkedlist at a index value
+    element_t *elementAtIndexInRange;
+    element_t *elementAtIndexOutOfRange; 
+    // Testing a valid index value
+    int indexInRange = 5; 
+    // Testing an invalid index value
+    int indexOutOfRange = 100;
+    printf("Now getting the elements at index %i %i\n", indexInRange, indexOutOfRange);
+    printf("Expected: Value at indexInRange = 150, Value at indexOutOfRange = NULL\n");
+    // Testing with an index in range
+    elementAtIndexInRange = list_get(testDoublyHeadedLL, indexInRange);
+    // Testing with an index out of range
+    elementAtIndexOutOfRange = list_get(testDoublyHeadedLL, indexOutOfRange);
+    // If get is not successful, return error
+    if (elementAtIndexInRange != NULL) { 
+        printf("index_get() succeeded for elementAtIndexInRange! The actual value is: %i\n", elementAtIndexInRange->val);
+    }  else { 
         printf("Something went wrong\n");
         return 1;
     }
 
-    // Attempting to print a destroyed Linked List 
-    printf("Now attempting to print a destroyed linked list, expected: NULL PARAM ERROR\n");
-    result_t actualResult = list_print(testDoublyHeadedLL); 
-    if (actualResult == NULL_PARAM) { 
-        printf("Actual result: NULL_PARAM\n\n");
+    // If get is not successful, return error
+    if (elementAtIndexOutOfRange == NULL) { 
+        printf("index_get() succeeded for elementAtIndexOutOfRange! The actual value is NULL\n\n"); 
     } else { 
-        printf("Actual result: OK\n");
+        printf("Something went wrong\n\n");
+        return 1;
+    }
+
+    
+    // Removing the first element from a linkedlist 
+    printf("Now testing the list_removeFront() function on testDoublyHeadedLL\n");
+    printf("Expected list: 9 elements and { 45 35 25 15 150 100 101 102 103 }\n");
+    // Save the result of removeFront to a variable
+    result_t removalResult = list_removeFront(testDoublyHeadedLL);
+    // If removeFront is not successful, return error
+    if (removalResult == OK) { 
+        printf("First element successfully removed...\n");
+        printf("Actual list is:\n"); 
+        list_print(testDoublyHeadedLL);
+        printf("\n");
+    } else { 
+        printf("Removal failed\n\n");
         return 1;
     }
 
 
+    // Testing append function with an empty linkedlist
+    printf("Now creating an empty list and appending one element to it\n");
+    // Create an empty linkedlist
+    list_t *emptyList = list_create();
+    // Append an element to the empty linkedlist
+    list_append(emptyList, 5);
+    list_print(emptyList);
+    printf("\n");
 
 
+    // Removing the first element from a linked list with only one element
+    printf("Now removing an element from a single element linked list\n");
+    printf("Expected list: 0 elements and { }\n");
+    // Save the result of removeFront to a variable
+    result_t removeFromSingleElement = list_removeFront(emptyList); 
+    // If removeFront is not successful, return error
+    if (removeFromSingleElement == OK) { 
+        printf("First element successfully removed...\n");
+        printf("Actual list is:\n"); 
+        list_print(emptyList);
+        printf("\n");
+    } else { 
+        printf("Removal failed\n\n");
+        return 1;
+    }
 
+
+    // Attemping to remove a element from a empty linkedlist
+    printf("Now removing an element from an empty linked list\n");
+    printf("Expect: ERROR\n");
+    // Save the result of removeFront to a variable
+    result_t removeFromEmpty = list_removeFront(emptyList); 
+    // If removeFront is not successful, return error
+    if (removeFromEmpty != OK) { 
+        printf("Actual: Function correctly did not try to remove from empty\n");
+        list_print(emptyList);
+        printf("\n");
+    } else { 
+        printf("Something went wrong\n\n");
+        return 1;
+    }
+
+    // Destroying a linkedlist
+    printf("Now testing list_destroy() function. Expected: success\n");
+    // Save the result of destroy to a variable
+    result_t destroyResult= list_destroy(emptyList);
+    // If destroy is not successful, return error
+    if (destroyResult == OK) { 
+        printf("Actual: Linked List successfully destroyed\n\n"); 
+    } else { 
+        printf("Actual: Something went wrong\n");
+        return 1;
+    }
+
+
+    // Attempting to print a destroyed Linked List 
+    emptyList = NULL;
+    printf("Now attempting to print a destroyed linked list, Expected: NULL PARAM ERROR\n");
+    // Save the result of print to a variable
+    result_t actualResult = list_print(emptyList); 
+    // If print is not as expected, return error
+    if (actualResult == NULL_PARAM) { 
+        printf("Actual: NULL_PARAM\n\n");
+    } else { 
+        printf("Actual: OK\n");
+        return 1;
+    }
 
     return 0;
 }
