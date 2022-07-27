8aa9858c9fe086f27986947c0bd2491a41ad8109
Steven Wong <syw14@sfu.ca>
2022-07-26 18:58:50 -0700

    Stack header file

--- Lab10/Stack.h
+++ Lab10/Stack.h
@@ -1,4 +1,11 @@
-// Header file for stack class
+ /* File: Stack.h
+ *
+ * Description: Implementation of a SHSL-based Stack of char with push/pop ...
+ * Class Invariant: ... in a LIFO order.
+ *
+ * Author: Steven Wong
+ * Date: July 26, 2022
+ */
 
 #pragma once
 #include "StackNode.h"
@@ -7,15 +14,26 @@
 class Stack { 
 
 private:
-
-
-
+    // Some Data Structure
+    char data;
+    StackNode *head;
 
 public: 
+    // Constructor 
+    Stack();
 
+    // Destructor 
+    ~Stack();
 
+    // Insert a new element onto the top of the stack
+    void push(char newElement);
 
+    // Remove and return the top most element of the stack
+    char pop(void); 
 
+    // Retrieve and return the top most element of the stack 
+    char peek(void) const; 
 
-
+    // Returns true if the Stack is empty and false otherwise
+    bool isEmpty(void) const;
 };
